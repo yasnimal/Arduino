2dd93b015401e44db0fc588fe8cb4029f17afe3d
yasnimal <ynimalasuriya@yahoo.com>
2016-08-29 17:18:44 +0530

    Add files via upload

---
+++ SimpleTimer/example/timerCheck/timerCheck.ino
@@ -0,0 +1,26 @@
+#include <SimpleTimer.h>
+
+SimpleTimer WorkTimer;
+SimpleTimer FastTimer;
+
+void setup() {
+  Serial.begin(115200);
+  WorkTimer.setInterval(5000);
+  FastTimer.setInterval(500);
+  WorkTimer.start();
+  FastTimer.start();
+}
+
+void loop() {
+
+  if (WorkTimer.isReady()) {
+    Serial.print("Work:");
+    Serial.println(millis());
+  }
+
+  if (FastTimer.isReady()) {
+    Serial.print("Fast:");
+    Serial.println(millis());
+  }
+
+}
