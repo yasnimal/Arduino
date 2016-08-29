218be0c35fe94c988f42b86308c105d907ea8704
yasnimal <ynimalasuriya@yahoo.com>
2016-08-29 17:17:04 +0530

    Add files via upload

---
+++ LineCamera/example/lineCameraTest/lineCameraTest.ino
@@ -0,0 +1,28 @@
+#include <LineCamera.h>
+
+const int syncPin = 31;  //camera SI pin, can not change
+const int clockPin = 30; //camera clk pin, can not change
+const int dataPin = 0; // must be in range 0 to 5  , analog pin,
+
+const int expos = 500;
+
+LineCamera cam(clockPin, syncPin, dataPin);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  cam.setExposure(expos);
+  cam.setBackground(cam.BG_WHITE);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  cam.capture(); 
+  int pos = cam.getEdgePos();
+  
+  Serial.println(pos);
+  delay(10);
+  
+}
